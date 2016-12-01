10e2cbc17ef348e79c88e74b6929acc96ef41b2f
sWat <s.Watanabe@watanabesakai-no-MacBook-Air.local>
2016-12-02 01:49:54 +0900

    投票ページの名前を変えた

---
+++ selePers.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<div style="background: #F0FFF0;">
+<title>投票システム</title>
+</head>
+<body>
+
+</body></div>
+</html>
