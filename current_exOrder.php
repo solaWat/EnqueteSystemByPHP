c9c397495a50c69aad3a0d6ed41f63623d52cd90
sWat <s.Watanabe@watanabesakai-no-MacBook-Air.local>
2016-12-12 16:46:57 +0900

    デバッグ用に名前を変更していたのを忘れていたので，元に戻す．

--- current_exOrder.php
+++ current_exOrder.php
@@ -5,12 +5,8 @@
 <title>現在の出席者と発表順</title>
 </head>
 <body>
-
-
 <?php
-
 $person = file('exOrder_prz.txt');
-
   print"<table border='1' cellpadding='5' style='background:#F0F8FF'>";
     print"<caption>発表順";
     
@@ -22,21 +18,7 @@ $person = file('exOrder_prz.txt');
     }
     print"</tr>";
   print"</table>";
-
-
-// // 投票結果表示
-// print"<table border='1' cellpadding='6' style='background:white'>";
-// for ($i = 0; $i < count($food); $i++) {
-//   $h = $i + 1 ;
-//   print "<tr>";
-//   print "<td>$h</td>";
-//   print "<td>{$food[$i]}</td>";
-//   print "</tr>\n";
-// }
-// print"</table>";
-
 ?>
-
 <br>
 <br><br><br>
 </body>
--- withTimer.php
+++ withTimer.php
@@ -7,22 +7,7 @@
 <body>
 <br>
 <?php
-
 include('current_exOrder.php');
-// $person = file('exOrder_prz.txt');
-
-//   print"<table border='1' cellpadding='5' style='background:#F0F8FF'>";
-//     print"<caption>発表順";
-    
-//     for ($i = 0; $i < count($person); $i++){ // 何位まで取得するか．
-//       $j = $i + 1; // 発表順を見せるための変数．
-//       print"<tr>";
-//       print "<td>$j.{$person[$i]}</td>";
-//       print"</tr>";
-//     }
-//     print"</tr>";
-//   print"</table>";
-
 include('timekeeper-gh-pages/index.html');
 ?>
 </body>
