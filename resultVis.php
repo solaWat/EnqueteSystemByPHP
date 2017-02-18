426c79d9720af78fdd685dcfecb7426d9b00e8a8
sWat <w1550038@edu.cc.uec.ac.jp>
2017-02-18 15:47:07 +0900

    集計結果ページにて，プレゼンの発表順を基準として，各人の被投票数集計を，グラフと合わせて，表示することに成功．

--- resultVis.php
+++ resultVis.php
@@ -3,7 +3,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <!-- 6秒ごとにページを自動更新 -->
-<META HTTP-EQUIV="Refresh" CONTENT="11"> 
+<!-- <META HTTP-EQUIV="Refresh" CONTENT="11">  -->
 <title>集計結果の表示</title>
 </head>
 <body>
@@ -12,8 +12,8 @@
 <img src="rest_nobita.jpg"></img><br><br><br>
 
 <?php
-date_default_timezone_set('Asia/Tokyo');
-$date = date('Y/m/d');
+// date_default_timezone_set('Asia/Tokyo');
+// $date = date('Y/m/d');
 
 $person = file('exOrder_prz.txt');
 $person_fg = file('exOrder_fg.txt'); // 結局，ファシグラの並び順はこのページでは使わなくなったが，一応残しておく．
@@ -39,6 +39,86 @@ $ee_one = $ee[count($person)-2];
       }
   }
 
+
+
+date_default_timezone_set('Asia/Tokyo');
+$date = date('Y-m-d');
+$time = date('H:i:s');
+
+$dbh = new PDO('mysql:host=127.0.0.1;charset=utf8',  root, root); //各々の環境で変わります．
+$dbh->query("USE enquete_main");
+
+$query = <<< EOM
+  SELECT studentname, person_id
+  FROM  TestA_2_lab_member_name
+  LEFT JOIN TestA_3_order_of_presen
+  ON TestA_2_lab_member_name.person_id = TestA_3_order_of_presen.attendee_person_id
+  WHERE TestA_3_order_of_presen.date = '$date'
+  ORDER BY TestA_3_order_of_presen.order_of_presen;
+EOM;
+$st = $dbh->query("$query");
+
+// $query = "SELECT studentname FROM TestA_2_lab_member_name WHERE person_id = '$fromSession' AND fiscal_year = '2016' ";
+// $st = $dbh->query("$query"); 
+
+foreach ($st as $row) {
+  $attendee_studentname[] = $row['studentname'];
+  $hoge[] = $row['person_id'];
+}
+
+$attendee_person_number = count($attendee_studentname);
+// $stmt = $pdo -> query("SELECT * FROM テーブル名");
+// $count = $stmt -> rowCount();
+
+
+// P票の集計
+for ($i=0; $i < count($hoge); $i++) { 
+  $one_person_id = $hoge[$i];
+// なんかサブクエリがうまくいかない
+$query = <<< EOM
+    SELECT 
+      COUNT(rank = '1' or null) AS rank1_num,
+      COUNT(rank = '2' or null) AS rank2_num,
+      COUNT(rank = '3' or null) AS rank3_num
+    FROM TestA_1_vote
+    WHERE date = '$date'
+     AND types_of_votes = 'P'
+     AND voted_person_id = '$one_person_id' ;
+EOM;
+  $st = $dbh->query("$query"); 
+
+  foreach ($st as $row) {
+    $sum_voted_P[] = ($row['rank1_num'] * 3)+($row['rank2_num'] * 2)+($row['rank3_num'] * 1);
+    // $sum_voted_P[] = $row['sum_voted'];
+  }
+}
+
+// FG票の集計
+for ($i=0; $i < count($hoge); $i++) { 
+  $one_person_id = $hoge[$i];
+// なんかサブクエリがうまくいかない
+$query = <<< EOM
+    SELECT 
+      COUNT(rank = '1' or null) AS rank1_num,
+      COUNT(rank = '2' or null) AS rank2_num,
+      COUNT(rank = '3' or null) AS rank3_num
+    FROM TestA_1_vote
+    WHERE date = '$date'
+     AND types_of_votes = 'FG'
+     AND voted_person_id = '$one_person_id' ;
+EOM;
+  $st = $dbh->query("$query"); 
+
+  foreach ($st as $row) {
+    $sum_voted_FG[] = ($row['rank1_num'] * 3)+($row['rank2_num'] * 2)+($row['rank3_num'] * 1);
+    // $sum_voted_P[] = $row['sum_voted'];
+  }
+}
+
+
+
+echo "現在，$attendee_person_number 人中『 $hito 人』の投票が終わっています．";
+
 // 投票済み者の数え上げ(enquete_przベース)
 for ($i = 0; $i < count($ed); $i++) {
   $sum += $ed[$i];
@@ -58,13 +138,13 @@ print"<table>";
     print"<caption align='left'>　　　　　　　　プレゼン";
     // 投票結果表示
     // プレゼンテーション
-    for ($i = 0; $i < count($person); $i++) {
+    for ($i = 0; $i < count($hoge); $i++) {
       print "<tr>";
-      print "<td style='background:white'>　{$person[$i]}　</td>";
+      print "<td style='background:white'>　{$attendee_studentname[$i]}　</td>";
       print "<td><table><tr>";
-      $w = $ed[$i] * 10;
+      $w = $sum_voted_P[$i] * 10;
       print "<td width='$w' bgcolor='green'> </td>";
-      print "<td>{$ed[$i]} 票</td>";
+      print "<td>{$sum_voted_P[$i]} 票</td>";
       print "</tr></table></td>";
       print "</tr>\n";
     }
@@ -79,13 +159,13 @@ print"<table>";
     print"<caption>ファシグラ";
     print"<tr>";
     // ファシグラ
-    for ($i = 0; $i < count($person_fg); $i++) {
+    for ($i = 0; $i < count($hoge); $i++) {
       print "<tr>";
       //print "<td>{$person_fg[$i]}</td>";
       print "<td><table><tr>";
-      $w = $ee[$i] * 10;
+      $w = $sum_voted_FG[$i] * 10;
       print "<td width='$w' bgcolor='green'> </td>";
-      print "<td>{$ee[$i]} 票</td>";
+      print "<td>{$sum_voted_FG[$i]} 票</td>";
       print "</tr></table></td>";
       print "</tr>\n";
     }
