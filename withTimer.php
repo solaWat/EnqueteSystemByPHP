844640790b0491b9fd7254f3496e9219557cc114
sWat <w1550038@edu.cc.uec.ac.jp>
2017-02-27 22:33:49 +0900

    「index.html」：beautifyでリンク先の文字列が切れ目が入っていたから，修正． 「mainvote.php」：エラーメッセージを吐かせたら，今まで無視してたものが吹き出してきた感じ．やばい． 「withTimer.php」：PHP記述を最小限にした．

--- withTimer.php
+++ withTimer.php
@@ -1,15 +1,12 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html lang="ja">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>タイマー</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <title>タイマー</title>
 </head>
 <body>
-<br>
-<?php
-echo'<br><br>';
-include 'current_exOrder.php';
-include 'timekeeper-gh-pages/index.html';
-?>
+  <br><br><br>
+  <?php include 'current_exOrder.php';?>
+  <?php include 'timekeeper-gh-pages/index.html';?>
 </body>
 </html
