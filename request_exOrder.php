1dd50cc4b0c2fc3ce7fb6e3bf6636376103e109e
sWat <w1550038@edu.cc.uec.ac.jp>
2017-03-14 11:28:15 +0900

    次のファイルの編集に手を出す．

--- request_exOrder.php
+++ request_exOrder.php
@@ -1,3 +1,27 @@
+<?php
+$dbname = 'enquete_main_2';//各々の環境で変わります．
+$pre_dsn = 'mysql:host=127.0.0.1;charset=utf8';
+$dsn = 'mysql:host=127.0.0.1;dbname='.$dbname.';charset=utf8mb4';//各々の環境で変わります．
+$user = 'root';//各々の環境で変わります．
+$password = 'root';//各々の環境で変わります．
+
+$tbname_1 = 'test_vote';
+$tbname_2 = 'test_lab_member_info';
+$tbname_3 = 'test_order_of_presentation';
+$fiscalyear = '2016'; // 今の所はとりあえず，年度に関しては，ベタ打ちとする．
+
+date_default_timezone_set('Asia/Tokyo');
+$date = date('Y-m-d');
+$time = date('H:i:s');
+
+try {
+
+
+} catch (Exception $e) {
+
+}
+
+?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html lang="ja">
 <head>
