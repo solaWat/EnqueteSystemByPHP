598c4314aa430ffd437656ab7830c7abd79d4bdd
sWat <w1550038@edu.cc.uec.ac.jp>
2017-03-22 17:16:21 +0900

    inputName.php に着手した．mainVoteに飛ばす処理があるため，とりあえず，次に行く．なぜかHTMLの作法が守られていない．何かわけがあるのかも．

--- inputName.php
+++ inputName.php
@@ -1,45 +1,123 @@
+<?php
+$dbname = 'enquete_main_2';//各々の環境で変わります．
+$pre_dsn = 'mysql:host=127.0.0.1;charset=utf8';
+$dsn = 'mysql:host=127.0.0.1;dbname='.$dbname.';charset=utf8mb4';//各々の環境で変わります．
+$user = 'root';//各々の環境で変わります．
+$password = 'root';//各々の環境で変わります．
+
+$tbname_1 = 'test_vote';
+$tbname_2 = 'test_lab_member_info';
+$tbname_3 = 'test_order_of_presentation';
+$fiscalyear = '2016'; // 今の所はとりあえず，年度に関しては，ベタ打ちとする．
+
+date_default_timezone_set('Asia/Tokyo');
+$date = date('Y-m-d');
+$time = date('H:i:s');
+
+try {
+  ini_set('session.gc_maxlifetime', 60 * 60 * 24 * 30);
+  ini_set('session.cookie_lifetime', 60 * 60 * 24 * 30); // クッキーを発行してから，(約？)30日間の有効期限を設定．
+  session_start(); // session_start() は、セッションを作成します。 もしくは、リクエスト上で GET, POST またはクッキーにより渡されたセッション ID に基づき現在のセッションを復帰します。
+
+  // トークンを発行する
+  $token = md5(uniqid(rand(), true));
+  // トークンをセッションに保存
+  $_SESSION['token'] = $token;
+
+  if (isset($_SESSION['my_id'])) { // 以前のセッション登録したことがある場合
+      header('Location: mainVote.php'); // 即，投票ページに飛ぶ．
+      exit();
+  }
+
+
+  $dbh = new PDO(
+    $dsn,
+    $user,
+    $password,
+    array(
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+      PDO::ATTR_EMULATE_PREPARES => false,
+    )
+  );
+
+
+  // 研究室所属メンバーを表示する．
+  $sql = 'SELECT * FROM '.$tbname_2.' WHERE fiscal_year = ? ';
+  $prepare_memberinfo = $dbh->prepare($sql);
+  //$prepare->bindValue(1, $tbname_2, PDO::PARAM_STR);
+  $prepare_memberinfo->bindValue(1, $fiscalyear, PDO::PARAM_STR);
+  $prepare_memberinfo->execute();
+
+
+
+
+
+} catch (Exception $e) {
+  header('Content-Type: text/plain; charset=UTF-8', true, 500);
+  echo 'エラー!: '.$e->getMessage().'<br/>';
+  die();
+}
+function h($str)
+{
+    return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+}
+header('Content-Type: text/html; charset=utf-8');
+?>
+
+
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>名前を選択</title>
+<h3>あなたの名前を教えてください．</h3>
 <?php
-// ini_set( 'session.save_path', '/var/tmp_enqueteSystem' );
-
-ini_set('session.gc_maxlifetime', 60 * 60 * 24 * 30);
-ini_set('session.cookie_lifetime', 60 * 60 * 24 * 30); // クッキーを発行してから，(約？)30日間の有効期限を設定．
-
-session_start(); // session_start() は、セッションを作成します。 もしくは、リクエスト上で GET, POST またはクッキーにより渡されたセッション ID に基づき現在のセッションを復帰します。
-
-// トークンを発行する
-$token = md5(uniqid(rand(), true));
-// トークンをセッションに保存
-$_SESSION['token'] = $token;
-
-if (isset($_SESSION['my_id'])) { // 以前のセッション登録したことがある場合
-    header('Location: mainVote.php'); // 即，投票ページに飛ぶ．
-    exit();
-}
+// // ini_set( 'session.save_path', '/var/tmp_enqueteSystem' );
+//
+// ini_set('session.gc_maxlifetime', 60 * 60 * 24 * 30);
+// ini_set('session.cookie_lifetime', 60 * 60 * 24 * 30); // クッキーを発行してから，(約？)30日間の有効期限を設定．
+//
+// session_start(); // session_start() は、セッションを作成します。 もしくは、リクエスト上で GET, POST またはクッキーにより渡されたセッション ID に基づき現在のセッションを復帰します。
+//
+// // トークンを発行する
+// $token = md5(uniqid(rand(), true));
+// // トークンをセッションに保存
+// $_SESSION['token'] = $token;
+//
+// if (isset($_SESSION['my_id'])) { // 以前のセッション登録したことがある場合
+//     header('Location: mainVote.php'); // 即，投票ページに飛ぶ．
+//     exit();
+// }
 ?>
 
 <!-- 自分の名前の登録は，遷移先で行われる． -->
 <form action="mainVote.php" method="post">
+  <?php foreach ($prepare_memberinfo as $row): ?>
+  <?php $name   = $row['studentname'];?>
+  <?php $id = $row['person_id'];?>
+      <label>
+        <input type='radio' name='my_id' value='<?=h($id)?>' checked><?=h($name)?>
+        <br><br>
+      </label>
+  <?php endforeach; ?>
+
 <?php
-echo '<h3>あなたの名前を教えてください．</h3>';
+//echo '<h3>あなたの名前を教えてください．</h3>';
 
-try {
-    $dbh = new PDO('mysql:host=127.0.0.1;charset=utf8',  root, root); //各々の環境で変わります．
-  $dbh->query('USE enquete_main');
-
-  // "fiscal_year"に関しては，後で，フロントサイドからトグル？などで「年度」を選択できるようにしたい．
-  $st = $dbh->query("SELECT * FROM TestA_2_lab_member_info WHERE fiscal_year = '2016'");
-
-    foreach ($st as $row) {
-        $name = $row['studentname'];
-        $id   = $row['person_id'];
-        echo "<label><input type='radio' name='my_id' value='$id' checked>{$name}<br><br></label>";
-    }
-} catch (PDOException $e) {
-    echo 'エラー!: '.$e->getMessage().'<br/>';
-    die();
-}
+// try {
+//     $dbh = new PDO('mysql:host=127.0.0.1;charset=utf8',  root, root); //各々の環境で変わります．
+//   $dbh->query('USE enquete_main');
+//
+//   // "fiscal_year"に関しては，後で，フロントサイドからトグル？などで「年度」を選択できるようにしたい．
+//   $st = $dbh->query("SELECT * FROM TestA_2_lab_member_info WHERE fiscal_year = '2016'");
+//
+//     foreach ($st as $row) {
+//         $name = $row['studentname'];
+//         $id   = $row['person_id'];
+//         echo "<label><input type='radio' name='my_id' value='$id' checked>{$name}<br><br></label>";
+// //     }
+// } catch (PDOException $e) {
+//     echo 'エラー!: '.$e->getMessage().'<br/>';
+//     die();
+// }
 
 ?>
 <input type="submit" value="送信する" onClick="return confirm('名前を再度確認したのち，[OK]を押してください．')" />
