6026982bb9b1f8323352898e89bbd525182683e8
sWat <s.Watanabe@watanabesakai-no-MacBook-Air.local>
2017-04-09 21:05:50 +0900

    deleteSess.php の記述をちょっと直す．DBとつなげない処理をするので，簡単だった．

--- deleteSess.php
+++ deleteSess.php
@@ -1,13 +1,23 @@
+<?php
+
+try {
+  session_start();
+
+  //session_unset(); // 特定のセッション変数のみを消したい場合に使うのが良いらしい．
+  // 以下の処理は，冗長という意見がある．
+  $_SESSION = array(); // セッション変数を，空の配列で初期化
+  setcookie(session_name(), '', time() - 3600, '/'); // ブラウザのクッキーを削除
+  session_destroy(); // サーバ側にあるセッションIDの破棄．
+
+} catch (Exception $e) {
+  header('Content-Type: text/plain; charset=UTF-8', true, 500);
+  echo 'エラー!: '.$e->getMessage().'<br/>';
+  die();
+}
+header('Content-Type: text/html; charset=utf-8');
+?>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>セッションCookieの消去</title>
-<?php
-session_start();
 
-//session_unset(); // 特定のセッション変数のみを消したい場合に使うのが良いらしい．
-// 以下の処理は，冗長という意見がある．
-$_SESSION = array(); // セッション変数を，空の配列で初期化
-setcookie(session_name(), '', time() - 3600, '/'); // ブラウザのクッキーを削除
-session_destroy(); // サーバ側にあるセッションIDの破棄．
-?>
 <p>消去に成功しました．</p>
 <p><a href="./index.html">TOP</a></p>
