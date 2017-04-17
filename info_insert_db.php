15128abb8a14c0e582d3907d7c3d420d335fbab6
sWat <s.Watanabe@watanabesakai-no-MacBook-Air.local>
2017-04-17 10:30:14 +0900

    info_insert_db.php という lab_member_info テーブルへの情報を自動入力してくれるページを追加．amazon linux でMySQLに日本語入力でハマったので，苦肉の策である．

---
+++ info_insert_db.php
@@ -0,0 +1,72 @@
+<?php
+$dbname   = 'enquete_main_2';//各々の環境で変わります．
+$pre_dsn  = 'mysql:host=127.0.0.1;charset=utf8';
+$dsn      = 'mysql:host=127.0.0.1;dbname='.$dbname.';charset=utf8mb4';//各々の環境で変わります．
+$user     = 'root';//各々の環境で変わります．
+$password = 'root';//各々の環境で変わります．
+
+$tbname_1   = 'test_vote';
+$tbname_2   = 'test_lab_member_info';
+$tbname_3   = 'test_order_of_presentation';
+$fiscalyear = '2017'; // 今の所はとりあえず，年度に関しては，ベタ打ちとする．
+
+date_default_timezone_set('Asia/Tokyo');
+$date = date('Y-m-d');
+$time = date('H:i:s');
+
+try {
+    $dbh = new PDO(
+      $dsn,
+      $user,
+      $password,
+      array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+      )
+    );
+  
+    $insert_info_sql = <<< EOM
+      INSERT IGNORE INTO `test_lab_member_info` (fiscal_year, studentname, person_id) VALUES
+      (2017, 'Ghita Athalina', '1701g'),
+      (2017, '安保　建朗', '1702a'),
+      (2017, '森田　和貴', '1703m'),
+      (2017, '渡辺　宇', '1704w'),
+      (2017, '荒木　香名', '1705a'),
+      (2017, '柴沢　弘樹', '1706s'),
+      (2017, '有竹　克馬', '1707a'),
+      (2017, '小山　祐希', '1708k'),
+      (2017, '持田　成一', '1709m'),
+      (2017, '吉田　梢', '1710y')
+EOM;
+    // $dbh->exec($insert_info_sql);
+    $prepare = $dbh->prepare($insert_info_sql);
+    $prepare->execute();
+
+} catch (PDOException $e) {
+    header('Content-Type: text/plain; charset=UTF-8', true, 500);
+    echo 'エラー!: '.$e->getMessage().'<br/>';
+    die();
+}
+function h($str)
+{
+    return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+}
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>初回INSERT</title>
+</head>
+<body>
+<p>
+  lab_member_info への登録に成功しました．
+</p>
+<p>
+  失敗した時のコピペ
+   DELETE FROM `test_lab_member_info` WHERE `fiscal_year` = '2017'
+</p>
+<a href= index.html > TOP </a>
+</body>
+</html>
