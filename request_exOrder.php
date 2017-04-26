74a94669f5b3b13aa2b65f9131a5f196bb76b27f
sWat <w1550038@edu.cc.uec.ac.jp>
2017-04-26 13:01:54 +0900

    var_conf.php というファイルを新たに作り，今まで各ファイルの先頭で宣言していた変数の記述を移した．各ファイルの先頭でvar_conf.php をinclude してくることで変数を使える．

--- request_exOrder.php
+++ request_exOrder.php
@@ -1,20 +1,6 @@
 <?php
-$dbname   = 'enquete_main_2';//各々の環境で変わります．
-$pre_dsn  = 'mysql:host=127.0.0.1;charset=utf8';
-$dsn      = 'mysql:host=127.0.0.1;dbname='.$dbname.';charset=utf8mb4';//各々の環境で変わります．
-$user     = 'root';//各々の環境で変わります．
-$password = 'root';//各々の環境で変わります．
-
-$tbname_1   = 'test_vote';
-$tbname_2   = 'test_lab_member_info';
-$tbname_3   = 'test_order_of_presentation';
-$tbname_4   = 'test_order_of_fg';
-$fiscalyear = '2017'; // 今の所はとりあえず，年度に関しては，ベタ打ちとする．
-
-
-date_default_timezone_set('Asia/Tokyo');
-$date = date('Y-m-d');
-$time = date('H:i:s');
+// 基本的な変数は var_conf ファイルを参照のこと．
+include ('var_conf.php');
 
 try {
   /**
