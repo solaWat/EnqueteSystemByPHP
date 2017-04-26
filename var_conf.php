da5bf04b54f6748dc0a009d123078126b2a6f44a
sWat <w1550038@edu.cc.uec.ac.jp>
2017-04-26 13:22:26 +0900

    var_conf.php において，適当なコメントを加えた．

--- var_conf.php
+++ var_conf.php
@@ -1,16 +1,64 @@
 <?php
-$dbname   = 'enquete_main_2';//各々の環境で変わります．
-$pre_dsn  = 'mysql:host=127.0.0.1;charset=utf8';
-$dsn      = 'mysql:host=127.0.0.1;dbname='.$dbname.';charset=utf8mb4';//各々の環境で変わります．
-$user     = 'root';//各々の環境で変わります．
-$password = 'root';//各々の環境で変わります．
+/**
+ * このプログラムが接続するデータベースの名前
+ * @var string
+ */
+$dbname   = 'enquete_main_2';
 
+
+/**
+ * 初めてデータベースに接続する際のみに用いる．
+ * これを使って，$dbname のデータベースを作る．
+ * @var string
+ */
+$pre_dsn  = 'mysql:host=127.0.0.1;charset=utf8';
+
+
+/**
+ * $dbname のデータベースを作成した後は，
+ * これを使ってデータベースにアクセスする．
+ * @var string
+ */
+$dsn      = 'mysql:host=127.0.0.1;dbname='.$dbname.';charset=utf8mb4';
+
+
+/**
+ * データベースに接続する際の，ユーザーネームとパスワード．
+ * 適宜，適切なものを設定すること．
+ * @var string
+ * @var string
+ */
+$user     = 'root';
+$password = 'root';
+
+
+/**
+ * 接続したデータベースにおけるテーブル名．
+ * 各々のデータベース設計については，
+ * exMember.php を参照のこと．
+ * @var string
+ * @var string
+ * @var string
+ * @var string
+ */
 $tbname_1   = 'test_vote';
 $tbname_2   = 'test_lab_member_info';
 $tbname_3   = 'test_order_of_presentation';
 $tbname_4   = 'test_order_of_fg';
-$fiscalyear = '2017'; // 今の所はとりあえず，年度に関しては，ベタ打ちとする．
 
+
+/**
+ * 現在における「年度」の宣言．
+ * データベースとやりとりする際にも使われる．
+ * 今の所はとりあえず，ベタ打ちとする．
+ * @var string
+ */
+$fiscalyear = '2017';
+
+
+/**
+ * 現在・現地における「日付」と「時間」
+ */
 date_default_timezone_set('Asia/Tokyo');
 $date = date('Y-m-d');
 $time = date('H:i:s');
