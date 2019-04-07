967ae8211991a2700ccf8e355a284872f71da967
EC2 Default User <ec2-user@ip-172-31-1-23.ap-southeast-1.compute.internal>
2019-04-07 06:21:25 +0000

    change view by bootStrap @resultVis.php

--- resultVis.php
+++ resultVis.php
@@ -181,23 +181,143 @@ header('Content-Type: text/html; charset=utf-8');
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html lang="ja">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<!-- 11秒ごとにページを自動更新 -->
-<META HTTP-EQUIV="Refresh" CONTENT="11">
-<title>集計結果の表示</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <!-- UTF-8 or Shift_JIS or EUC-JP -->
+  <!-- 11秒ごとにページを自動更新 -->
+  <META HTTP-EQUIV="Refresh" CONTENT="11">
+	
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>集計結果の表示</title>
+  <!-- BootstrapのCSS読み込み -->
+  <link href="bootstrap-4.3.1-dist/css/bootstrap.min.css" rel="stylesheet">
+  <!-- jQuery読み込み -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <!-- BootstrapのJS読み込み -->
+  <script src="bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
+  <!-- iconの読み込み　外部サイト：「Font Awesome」-->
+  <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js" integrity="sha384-0pzryjIRos8mFBWMzSSZApWtPl/5++eIfzYmTgBBmXYdhvxPc+XcFEk+zJwDgWbP" crossorigin="anonymous"></script>
 </head>
 <body>
-お疲れ様でした．投票結果は自動的に更新されます．<br><br>
+  
+  <?php include ('header_general.php'); ?>
+  
+  <div class="media border p-3">
+    <img src="rest_nobita.jpg" alt="rest_nobita" class="mr-3 mt-3" style="width:120px;">
+    <div class="media-body">
+      <h5>お疲れ様でした．投票結果は自動的に更新されます</h5>
+    </div>
+  </div>
+  
+  <div class="row">
+    <div class="col">
+      
+    </div>
+    <div class="col-5 text-center btn shadow">
+      <h3>投票状況</h3>
+      <div class="card-deck">
+        <div class="card">
+          <div class="progress">
+            <div class="progress-bar progress-bar-striped" style="width:<?=h(100 * $finish_vote_num / $attendee_person_number)?>%">
+              <?=h($finish_vote_num)?> 人
+            </div>
+            <div class="progress-bar bg-secondary" style="width:<?=h(100 - 100 * $finish_vote_num / $attendee_person_number)?>%">
+              <?=h($attendee_person_number - $finish_vote_num)?> 人
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      
+    </div>
+  </div>
+  
+  <div class="row mt-5">
+    <div class="col">
+      
+    </div>
+    <div class="col-8 text-center shadow">
+      <div class="col text-center p-3">
+        <h3>投票結果（ <?=h($date)?> )</h3>
+        <div class="card-deck">
+          <div class="card table-responsive border border-info">
+            <div class="card-header">プレゼンテーション</div>
+            <div class="card-body mx-auto">
+              <table class="table table-bordered" style='background:#F0F8FF'>
+                <?php for ($i = 0; $i < count($id_order); ++$i) { ?>
+                  <tr>
+                    <td class="text-center" style="background:white">
+                      <h4>
+                        <?=h($attendee_studentname[$i])?>
+                      </h4>
+                    </td>
+                    <td>
+                      <table class="table table-sm table-borderless my-auto">
+                        <tr>
+                          <?php $w = $sum_voted_P[$i] * 10; ?>
+                          <td width=<?= $w ?> class="shadow" bgcolor='green'>
+                          </td>
+                          <td>
+                            <?=h($sum_voted_P[$i])?> 票
+                          </td>
+                        </tr>
+                      </table>
+                    </td>
+                  </tr>
+                <?php } ?>
+              </table>
+            </div>
+          </div>
+      
+          <div class="card responsive border border-secondary">
+            <div class="card-header">ファシグラ</div>
+            <div class="card-body mx-auto">
+              <table class="table table-bordered" style="background:#F5F5F5">
+                <?php for ($i = 0; $i < count($id_order_fg); ++$i) { ?>
+                  <tr>
+                    <td class="text-center" style="background:white">
+                      <h4>
+                        <?=h($attendee_studentname_fg[$i])?>
+                      </h4>
+                    </td>
+                    <td>
+                      <table class="table table-sm table-borderless my-auto">
+                        <tr>
+                          <?php $w = $sum_voted_FG[$i] * 10; ?>
+                          <td width=<?= $w ?> class="shadow" bgcolor='green'>
+                          </td>
+                          <td>
+                            <?=h($sum_voted_FG[$i])?> 票
+                          </td>
+                        </tr>
+                      </table>
+                    </td>
+                  </tr>
+                <?php } ?>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      
+    </div>
+  </div>
+  
+  
+  
 <!-- 休憩してて良いことを示す画像 -->
-<img src="rest_nobita.jpg"></img><br><br><br>
+<!--<img src="rest_nobita.jpg"></img><br><br><br>-->
 
-<p>
-  現在，『 <?=h($finish_vote_num)?> 人 』の投票が終わっています.
-</p>
-<p>
-  （発表した人の数は <?=h($attendee_person_number)?> 人です．）
-</p>
-<br><br>
+<!--<p>-->
+<!--  現在，『 <?=h($finish_vote_num)?> 人 』の投票が終わっています.-->
+<!--</p>-->
+<!--<p>-->
+<!--  （発表した人の数は <?=h($attendee_person_number)?> 人です．）-->
+<!--</p>-->
+<!--<br><br>-->
 
 <!-- 2つのテーブルと並列表示させるための透明テーブル -->
 <table>
@@ -265,18 +385,14 @@ header('Content-Type: text/html; charset=utf-8');
 </p>
 <br><br>
 <p>
-  <font color="brue">「shift」+「command」+「4」で，範囲を指定して，投票結果をスクリーンショットしてください．(mac)</font>
-</p><br><br>
-<a href= index.html > TOP </a>
-
-<br><br><br><br><br><br><br><br><br><br>
+  <font color="brue">「shift」+「command」+「4」で，範囲を指定して，投票結果をスクリーンショットしてください．(macの場合)</font>
+</p>
 
 <form method="post" action="resultVis.php">
   <input type="submit" name="delete_result" value="※押すな※　本日の投票データを全て削除　※">
 </form>
-<p>
-  <font color="red">管理人のつぶやき「なんか，全体的に殺風景だ……」</font>
-</p>
+
+  <?php include ('footer_general.php'); ?>
 
 </body>
 </html>
